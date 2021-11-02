73f64b7b74f01279bcf8e3e08622f6289ed3dd3d
slav-spice <88583009+slav-spice@users.noreply.github.com>
2021-11-02 11:40:01 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter = 0;
+function jump() {
+  if (character.classList == "animate") {
+    return;
+  }
+  character.classList.add("animate");
+  setTimeout(function() {
+    character.classList.remove("animate");
+  }, 300);
+}
+
+document.getElementById("showImage").onclick = function() {
+    document.getElementById("theImage").style.visibility = "visible";
+}
